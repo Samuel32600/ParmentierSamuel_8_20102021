6f83869114d8327db0a942f0a1005fa1bd79cd94
Samuel32600 <samuel.parmentier@orange.fr>
2021-10-20 17:08:38 +0200

    creation et mise en place du component Footer

--- kasa/src/components/App.js
+++ kasa/src/components/App.js
@@ -2,12 +2,17 @@ import React from 'react'
 // import logo from './logo.svg'
 import '../styles/App.css'
 import Header from './Header'
+import Footer from './Footer'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header/>
+
+
+        
+        <Footer/>
       </div>
     )
   }
---
+++ kasa/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo_footer from '../assets/logo_footer.png'
+import '../styles/footer.css'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div className='container-footer'>
+                <img src={logo_footer} className='logo-footer' alt='logo Kasa de fin de page'/>
+                <p>© 2020 Kasa. All rights reserved</p>
+            </div>
+        )
+    }
+}
+export default Footer
