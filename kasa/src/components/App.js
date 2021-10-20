50b7ec6d66499ba21445372dacf34d97d3fe231b
Samuel32600 <samuel.parmentier@orange.fr>
2021-10-20 15:53:22 +0200

    css du header + font family importer

--- kasa/src/components/App.js
+++ kasa/src/components/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
 // import logo from './logo.svg'
-import './styles/App.css'
+import '../styles/App.css'
 import Header from './Header'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        console.log(Header)
-        <Header/>
+        <Header />
       </div>
     )
   }
--- kasa/src/components/Header.js
+++ kasa/src/components/Header.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import logo_header from '../assets/logo_header.png'
+import '../styles/header.css'
 
 class Header extends React.Component {
     render() {
-        return
-         <div className='containerheader'>
-             <img src={logo_header} className='logo_header' alt='logo Kasa'/>
-             <nav>
-                 <ul>
-                     <li>Acceuil</li>
-                     <li>A Propos</li>
-                 </ul>
-             </nav>
-        </div>
+        return (
+            <div className='container-header'>
+                <img src={logo_header} className='logo-header' alt='logo Kasa' />
+                <nav>
+                    <ul className='nav-header'>
+                        <li className='border'>Acceuil</li>
+                        <li className='border'>A Propos</li>
+                    </ul>
+                </nav>
+            </div>
+        )
     }
 }
 
