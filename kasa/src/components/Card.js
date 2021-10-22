93d8d5eba9e5b4e8943c1bf80d5158884ea4690f
Samuel32600 <samuel.parmentier@orange.fr>
2021-10-22 18:19:50 +0200

    creation carte logement

---
+++ kasa/src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/card.css'
+
+class Card extends React.Component {
+    
+    render() {
+        return (
+            <div className='container-card'>
+                
+            </div>
+        )
+    }
+}
+
+export default Card
--- kasa/src/pages/Home-page.js
+++ kasa/src/pages/Home-page.js
@@ -6,15 +6,17 @@ import Banner_Home_Page from '../assets/banner_home-page.png'
 import Banner from '../components/Banner.js'
 let InfoBanner = "Chez vous, partout et ailleurs"
 
+//carte logement
+import Cards from '../components/Card.js'
+
 class Home_Page extends React.Component {
     render() {
         return (
 
             <div>
                 <Banner picture={Banner_Home_Page} text={InfoBanner} />
-
+                <Cards/>
             </div>
-
         )
     }
 }
