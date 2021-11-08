27a114030e490c0ff303daaba56ec5b07614e243
Samuel32600 <samuel.parmentier@orange.fr>
2021-11-08 09:51:10 +0100

    activation de l'image suivante

--- kasa/src/components/Carrousel.js
+++ kasa/src/components/Carrousel.js
@@ -13,16 +13,32 @@ class Carrousel extends React.Component {
     constructor(props) {
         super(props);
         this.picture = this.props.photography;        
+        this.lengthArray = this.props.photography.length;
+
         this.state = {arrayIndex :0}
-        this.lengthArray = this.props.photography.length
     }
+
+    //fonction image suivante
+    nextPicture(){
+        let activePicture = this.state.arrayIndex
+        let arrayLength = this.lengthArray
+
+        if(activePicture<arrayLength){
+            activePicture +=1
+        }
+        this.setState(
+            {arrayIndex: activePicture}
+        )
+    }
+
+    
    
     render() {
         return (
             <div className='container-carrousel'>
                 <img src={this.picture[this.state.arrayIndex]} className='picture-carrousel' alt='image du logement' />
                 <img src={Logo_previous} className='logo-previous' alt='feche pour image precedente' />
-                <img src={Logo_next} className='logo-next' alt='feche pour image suivante' />
+                <img src={Logo_next} className='logo-next' alt='feche pour image suivante' onClick={this.nextPicture.bind(this)}/>
                 <p className='text-picture'>{((this.state.arrayIndex)+1)}/{this.lengthArray}</p>
             </div>
         )
